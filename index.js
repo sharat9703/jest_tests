4cc1086ef1a838e01ceb02ba50628cb12fb57673
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 <sharath.yaragatti@yahoo.com>
2024-08-08 11:17:39 +0530

    files added

---
+++ index.js
@@ -0,0 +1,76 @@
+const axios = require("axios");
+
+async function get_all() {
+  try {
+    const response = await axios.get(`http://localhost:8000/api/customers`);
+    return response;
+  } catch (error) {
+    console.log("Error fetching items:", error.message);
+    throw error; 
+  }
+}
+
+async function get_one(reqBody) {
+  try {
+    const response = await axios.get(`http://localhost:8000/api/customer/${reqBody.cust_id}`);
+    return response;
+  } catch (error) {
+    console.log( error.message);
+    throw error; 
+  }
+}
+
+async function create_(reqBody) {
+  try {
+    const data = {
+      name: reqBody.name,
+      cust_id: reqBody.cust_id,
+      email: reqBody.email,
+    };
+    const response = await axios.post(`http://localhost:8000/api/customer`, data);
+    return response;
+  } catch (error) {
+    console.log(error.message);
+    if(error.response){
+       return {status: error.response.status}
+    } else{
+      return {status : 500}
+  }
+}
+}
+async function update_(reqBody) {
+  try {
+    const data = {
+      name: reqBody.name,
+      cust_id: reqBody.cust_id,
+      email: reqBody.email,
+    };
+    const response = await axios.put(`http://localhost:8000/api/customer/${data.cust_id}`, data);
+    return response;
+  } catch (error) {
+    console.log(error.message);
+    throw error; 
+  }
+}
+
+async function delete_(reqBody) {
+  try {
+    const response = await axios.delete(`http://localhost:8000/api/customer/${reqBody.cust_id}`);
+    return response;
+  } catch (error) {
+    console.log(error.message);
+    if (error.response) {
+      return { status: error.response.status };
+    } else {
+      return { status: 500 }; 
+    }
+  }
+}
+
+module.exports = {
+  get_all,
+  create_,
+  get_one,
+  update_,
+  delete_,
+};
---
+++ tests/test.js
@@ -0,0 +1,157 @@
+const {
+  get_all,
+  get_one,
+  create_,
+  update_,
+  delete_,
+} = require("../index");
+
+describe("API End Points GET Tests", () => {
+  test("should get all customers", async () => {
+    try {
+      const response = await get_all();
+      expect(response.status).toBe(200);
+      expect(response.data.status).toBe("success");
+      expect(response.data.data.length).toBeGreaterThan(0);
+    } catch (error) {
+      console.log(error.message);
+      throw error; 
+    }
+  });
+
+  test("should return 404 status for non-existent customer", async () => {
+    try {
+      const reqBody = {
+        name: "name",
+        cust_id: 99999,
+        email: "email@emial.com",
+      };
+      const response = await get_one(reqBody);
+      expect(response.status).toBe(404);
+    } catch (error) {
+      expect(error.response.status).toBe(404);
+    }
+  });
+
+  test("should get a customer by cust_id", async () => {
+    try {
+      const reqBody = {
+        name: "newguest",
+        cust_id: 51,
+        email: "newguest@gmail.com",
+      };
+      const response = await get_one(reqBody);
+      expect(response.status).toBe(200);
+      expect(response.statusText).toBe("OK");
+      expect(response.data.data.length).toBeGreaterThan(0);
+    } catch (error) {
+      console.log(error.message);
+      throw error; 
+    }
+  });
+});
+
+describe("API End Points POST tests", () => {
+  test("should create a new customer", async () => {
+    try {
+      const reqBody = {
+        name: "newguest70",
+        cust_id: 50,
+        email: "newguest70@gmail.com",
+      };
+      const response = await create_(reqBody);
+      expect(response.status).toBe(201);
+      expect(response.data.status).toBe("success");
+    } catch (error) {
+      console.log(error.message);
+      throw error; 
+    }
+  });
+
+  test("should return 409 for trying to create an existing record", async () => {
+    try {
+      const reqBody = {
+        name: "newguest9",
+        cust_id: 70,
+        email: "newguest6@gmail.com",
+      };
+      await create_(reqBody);
+    } catch (error) {
+      expect(error.response.status).toBe(409);
+    }
+  });
+
+  test("should return 400 for invalid customer data", async () => {
+    try {
+      const reqBody = { name: "sharat" };
+      await create_(reqBody);
+    } catch (error) {
+      expect(error.response.status).toBe(400);
+    }
+  });
+});
+
+describe("API End Points PUT tests", () => {
+  test("should update customer with given cust_id", async () => {
+    try {
+      const reqBody = {
+        name: "newguest",
+        cust_id: 51,
+        email: "newguest@gmail.com",
+      };
+      const response = await update_(reqBody);
+      expect(response.status).toBe(200);
+      expect(response.statusText).toBe("OK");
+      expect(response.data.message).toBe("customer updated successfully");
+    } catch (error) {
+      console.log(error.message);
+      throw error; // Rethrow to fail the test
+    }
+  });
+
+  test("should return 404 when updating non-existent customer", async () => {
+    try {
+      const reqBody = {
+        name: "newguest",
+        cust_id: 99999,
+        email: "newguest@gmail.com",
+      };
+      const response = await update_(reqBody);
+      // This should not be reached if an error is thrown
+      expect(response.status).toBe(404);
+    } catch (error) {
+      expect(error.response.status).toBe(404);
+    }
+  });
+});
+
+describe("API End Point DELETE tests", () => {
+  test("should delete a customer with given cust_id", async () => {
+    try {
+      const reqBody = {
+        cust_id: 50,
+      };
+      const response = await delete_(reqBody);
+      expect(response.status).toBe(200);
+      expect(response.statusText).toBe("OK");
+      expect(response.data.message).toBe("customer deleted successfully");
+    } catch (error) {
+      console.log(error.message);
+      throw error; // Rethrow to fail the test
+    }
+  });
+
+  test("should return 404 when deleting non-existent customer", async () => {
+    try {
+      const reqBody = {
+        cust_id: 999,
+      };
+      const response = await delete_(reqBody);
+      // This should not be reached if an error is thrown
+      expect(response.status).toBe(404);
+    } catch (error) {
+      console.log(error);
+      expect(error.response.status).toBe(404);
+    }
+  });
+});
